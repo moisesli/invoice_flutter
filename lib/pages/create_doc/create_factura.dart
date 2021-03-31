433a6fd0cf8b2d2ebb79f9d9fd2ff7a42157308c
moiseslo <elnaufrago2009@gmail.com>
2021-03-31 00:52:26 -0500

    edit

--- lib/pages/create_doc/create_factura.dart
+++ lib/pages/create_doc/create_factura.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:invoice/widgets/drawer.dart';
 
@@ -24,8 +26,106 @@ class _CreateFacturaState extends State<CreateFactura> {
         title: Text('Create Factura'),
       ),
       drawer: WidgetDrawer(),
-      body: Center(
-        child: Text('Crear Factura'),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 20.0, left: 10, right: 10),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'Numero y Serie'),
+            ),
+            SizedBox(height: 10.0),
+            TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'Razon Social'),
+            ),
+            SizedBox(height: 10),
+            TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'Documento'),
+            ),
+            SizedBox(height: 10),
+            TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'Fecha'),
+            ),
+            SizedBox(height: 10),
+            Expanded(
+              child: ListView(
+                children: <Widget>[
+                  Card(
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        child: Text(
+                          'P',
+                          style: TextStyle(fontSize: 28),
+                        ),
+                      ),
+                      title: Row(
+                        children: [
+                          Text('Arroz Costenio Graneadito 600 Gg')                          
+                        ],
+                      ),
+                      subtitle: Text('S/ 45.56 X 200 und'),
+                      trailing: Icon(Icons.delete),
+                    ),
+                  ),
+                  Card(
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        child: Text(
+                          'P',
+                          style: TextStyle(fontSize: 28),
+                        ),
+                      ),
+                      title: Row(
+                        children: [
+                          Text('Arroz Costenio Graneadito 600 Gg')
+                        ],
+                      ),
+                      subtitle: Text('S/ 45.56 X 200 und'),
+                      trailing: Icon(Icons.delete),
+                    ),
+                  ),
+                  Card(
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        child: Text(
+                          'P',
+                          style: TextStyle(fontSize: 28),
+                        ),
+                      ),
+                      title: Row(
+                        children: [
+                          Text('Arroz Costenio Graneadito 600 Gg')
+                        ],
+                      ),
+                      subtitle: Text('S/ 45.56 X 200 und'),
+                      trailing: Icon(Icons.delete),
+                    ),
+                  ),
+                  Card(
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        child: Text(
+                          'P',
+                          style: TextStyle(fontSize: 28),
+                        ),
+                      ),
+                      title: Row(
+                        children: [
+                          Text('Arroz Costenio Graneadito 600 Gg')
+                        ],
+                      ),
+                      subtitle: Text('S/ 45.56 X 200 und'),
+                      trailing: Icon(Icons.delete),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
