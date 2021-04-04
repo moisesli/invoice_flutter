b315401e224c6af3306c2f33165d86f197f9ff98
moiseslo <elnaufrago2009@gmail.com>
2021-04-04 01:24:26 -0500

    edit

--- lib/pages/create_doc/create_nota_cred.dart
+++ lib/pages/create_doc/create_nota_cred.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:invoice/widgets/data_search.dart';
 import 'package:invoice/widgets/drawer.dart';
 
 class CreateNotaCredito extends StatefulWidget {
@@ -8,13 +9,24 @@ class CreateNotaCredito extends StatefulWidget {
 
 class _CreateNotaCreditoState extends State<CreateNotaCredito> {
   @override
+  void initState() {
+    loadJson().then((value) {
+      setState(() {
+        data = value;
+      });
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      initialIndex: 1,
+      initialIndex: 0,
       length: 3,
       child: Scaffold(
         appBar: AppBar(
-          title: Text('Create Nota Credito'),
+          title: Text('Total S/ 405.67  Nota Credito Electronica',
+              style: TextStyle(fontSize: 18)),
           bottom: TabBar(
             tabs: [
               Tab(
@@ -28,17 +40,74 @@ class _CreateNotaCreditoState extends State<CreateNotaCredito> {
               )
             ],
           ),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () =>
+                  showSearch(context: context, delegate: DataSearch()),
+            )
+          ],
         ),
         drawer: WidgetDrawer(),
         body: TabBarView(
           children: [
-            Icon(Icons.directions_car),
+            Container(
+              child: ListView(
+                children: [
+                  Card(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        ListTile(
+                          leading: CircleAvatar(
+                            child: Image.asset('img/aceite_primor.jpg'),
+                          ),
+                          title: Text('Aceite Primor Vegetal Limpio'),
+                          subtitle: Wrap(
+                            children: [
+                              Text('Cantidad 1 Kg '),
+                            ],
+                          ),
+                          trailing: Text(
+                            'S/ 1 534.68',
+                            style:
+                                TextStyle(fontSize: 17, color: Colors.black87),
+                          ),
+                          onTap: () {},
+                        ),
+                      ],
+                    ),
+                  ),
+                  Card(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        ListTile(
+                          leading: CircleAvatar(
+                            child: Image.asset('img/costeno1.jpg'),
+                          ),
+                          title: Text('Arroz Costeno Graneadito Casero'),
+                          subtitle: Wrap(
+                            children: [
+                              Text('Cantidad 5 UND '),
+                            ],
+                          ),
+                          trailing: Text(
+                            'S/ 34 124.43',
+                            style:
+                            TextStyle(fontSize: 17, color: Colors.black87),
+                          ),
+                          onTap: () {},
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
             Icon(Icons.directions_transit),
             Icon(Icons.directions_bike),
           ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
         ),
       ),
     );
---
+++ lib/widgets/data_search.dart
@@ -0,0 +1,72 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+var data;
+Future loadJson() async {
+  String data = await rootBundle.loadString('assets/json/productos.json');
+  return json.decode(data);
+}
+
+class DataSearch extends SearchDelegate {
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    // TODO: implement buildActions
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      )
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    // TODO: implement buildLeading
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // TODO: implement buildResults
+    return Column(
+      children: [Text('data')],
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // TODO: implement buildSuggestions
+    var temporal = new List();
+    var temp = new List();
+    for (int i = 0; i < data.length; i++) {
+      temporal.add(data[i]);
+    }
+    temp = temporal
+        .where((note) => note['nombre'].toLowerCase().contains(query))
+        .toList();
+    print(temp);
+    return ListView.builder(
+      itemCount: temp.length,
+      itemBuilder: (context, int index) {
+        return Card(
+          child: ListTile(
+            leading: CircleAvatar(
+              radius: 20,
+              child: Text('P'),
+            ),
+            title: Text(temp[index]['nombre']),
+            onTap: () {},
+          ),
+        );
+      },
+    );
+  }
+}
